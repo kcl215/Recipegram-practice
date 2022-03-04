7d18e9a57ead4414c9c8a2acd4be38fadbe207a9
EC2 Default User <kcl988780@gmail.com>
2022-03-04 11:06:56 +0000

    [add]ユーザー編集機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2d6f57b6bca7d4f78126cbea34574569d307ee1c880d491b974abe6636a27ef7a38aa454ba6e30bfc68ea5e49423594bd8b31f2723111d95de72d4789ddc205c'
